744f69e6d2ef70a4e822e4b7e5a416f1abc20de9
Yousef Al-Akhali <aytpdh@inf.elte.hu>
2022-10-17 11:17:44 +0200

    Added the functions to seed.py inorder to create/remove dirs for each new fake user and fake project

--- backend/API/core/management/commands/seed.py
+++ backend/API/core/management/commands/seed.py
@@ -5,6 +5,8 @@ from django.core.management.base import BaseCommand
 
 from ...models import User, Project
 from ...tests.factories import UserFactory, ProjectFactory
+from ...utils import make_dir_for_project_of_user, make_dir_for_user, remove_dirs_of_user
+ 
 
 NUM_OF_FAKE_USERS = 10
 NUM_OF_FAKE_PROJECTS = 50
@@ -14,9 +16,18 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **kwargs):
+        
         self.stdout.write("Deleting old data...")
         models = [User, Project]
+        tmpBoolForUsers = True
+        
         for m in models:
+            
+            if(tmpBoolForUsers):
+                for aUser in m.objects.all():
+                    remove_dirs_of_user(str(aUser))
+                tmpBoolForUsers = False
+                
             m.objects.all().delete()
 
         self.stdout.write("Creating new data...")
@@ -25,10 +36,12 @@ class Command(BaseCommand):
         for _ in range(NUM_OF_FAKE_USERS):
             person = UserFactory()
             people.append(person)
+            make_dir_for_user(person.email)
 
         # Add some users to clubs
         for _ in range(NUM_OF_FAKE_PROJECTS):
             owner = random.choice(people)
             project = ProjectFactory(owner=owner)
+            make_dir_for_project_of_user(owner.email, project.name)
         
         self.stdout.write(self.style.SUCCESS('Successfully seeded fake data into the database'))
